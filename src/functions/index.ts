198022acc21f56696cc8db0f35554ed36a06362f
Felipe Fontoura <ffontouras@gmail.com>
2023-07-19 00:00:11 -0300

    adjusted index

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,24 +1,23 @@
+import { today } from './date/today'
+
 import { githubActionStatus } from './github/githubActionStatus'
 import { githubPullRequests } from './github/githubPullRequests'
 
 import { daily } from './sprint/daily'
+import { stories } from './sprint/stories'
 
 import { data } from './me/data'
 import { contacts } from './me/contacts'
 
 import { send } from './email/send'
 
-import { today } from './date/today'
-
-// import { lights } from "./home/lights"
-
 export const functionDeclarations = [
+  today,
   githubActionStatus,
   githubPullRequests,
   data,
   contacts,
   daily,
+  stories,
   send,
-  // lights
-  today,
 ]
