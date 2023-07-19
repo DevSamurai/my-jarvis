3b12b84b18cc462b741ba5e51c087fff6dc908f5
Felipe Fontoura <ffontouras@gmail.com>
2023-07-18 23:58:56 -0300

    adjusted today function

--- src/functions/date/today.ts
+++ src/functions/date/today.ts
@@ -3,14 +3,14 @@ import { FunctionDeclaration } from '../../interfaces/FunctionDeclaration'
 async function callback(): Promise<string> {
   console.debug('▶ today', {})
 
-  const response = { date: new Date().toISOString() }
+  const response = { date: new Date().toString() }
 
   return JSON.stringify(response)
 }
 
 export const today: FunctionDeclaration = {
   name: 'today',
-  description: 'Retorna a data e hora atual.',
+  description: 'Retorna a data e hora de hoje.',
   parameters: {
     type: 'object',
     properties: {},
